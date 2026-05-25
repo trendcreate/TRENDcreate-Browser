a1653dfcd695754363ab95807ffc04e665c76ca2
higashiikenaga <sanyokai05@gmail.com>
2026-05-25 15:15:34 +0900

    chore: bump version and remove temporary test files and diff logs

--- test-headless.js
+++
@@ -1,9 +0,0 @@
-const { app, BrowserWindow } = require('electron');
-app.whenReady().then(() => {
-  const win = new BrowserWindow({ show: false, webPreferences: { nodeIntegration: true, contextIsolation: false, webviewTag: true } });
-  win.webContents.on('console-message', (e, level, msg, line, sourceId) => {
-    console.log('[CONSOLE]', sourceId + ':' + line, msg);
-  });
-  win.loadFile('src/index.html');
-  setTimeout(() => app.quit(), 2000);
-});
